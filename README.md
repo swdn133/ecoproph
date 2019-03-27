26a9a1e55eb6916790c52287761e2addc2f6e1c0
Sebastian Widmann <widmann3@hm.edu>
2019-03-27 20:41:58 +0100

    first creation dataset resolution reduction

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Energy Consumption Prophet - A Machine Learning based Energy Forecast
 
 <img src="https://github.com/giftzwerg133/ecoproph/blob/master/resource/logo.png" width="400">
+
+## Reduce the resolution from the dataset from seconds to minutes
+```
+python src/reduce_dataset_minutes.py --path R:/ecoproph/Dataset/2018
+```
+- new file is .csv format
+- new file starts with header
+- resolution is now minutes
+
+Depending on the number of files this procedure can be pretty time consuming!
+
---
+++ src/reduce_dataset_minutes.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import pandas as pd
+import time
+
+def reduce_dataset_minutes(directory):   
+    os.makedirs(directory+"\\minutes", exist_ok=True)
+    cnt = 0
+    for filename in os.listdir(directory):
+        # only consider the dataset files
+        if filename.endswith(".dat"):
+            file = os.path.join(directory, filename)
+            start = time.time()
+            with open(file) as f:
+                cnt += 1 # just to track progress
+                header_line = 0
+                for line in f.readlines():
+                    # Find the header_line to parse the csv 
+                    if (line[0:8] == "YYYYMMDD"):
+                        break;  # found header
+                    header_line += 1
+                df = pd.read_csv(file, sep=';\t', header=header_line, engine='python')
+                print("{} - {}s".format(cnt, time.time()-start))
+                # every second must have the same format
+                df['hhmmss'] = df['hhmmss'].astype(str).str.zfill(6)
+                df['secs'] = df['hhmmss'].astype(str).str[4:]
+                # drop unnecessary 59 seconds - just keep the first second
+                df = df[df.secs == '00']
+                newfile = directory + "\\minutes\\" + filename[:-8] + "_min.csv"
+                print(newfile)
+                # save the resulting file
+                df.to_csv(newfile, sep=';')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, default="", help="Path with .dat files")
+    args = parser.parse_args()
+
+    assert os.path.isdir(args.path), "Path does not lead to a directory"
+    # run the main function
+    reduce_dataset_minutes(args.path)
