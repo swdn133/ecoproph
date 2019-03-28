5f8c868cb919b014a9cd26a557670a4aebfcbe73
Sebastian Widmann <widmann3@hm.edu>
2019-03-28 17:43:46 +0100

    added error handling

--- src/reduce_dataset_minutes.py
+++ src/reduce_dataset_minutes.py
@@ -1,35 +1,44 @@
 import argparse
 import os
 import pandas as pd
+import sys
 import time
 
 def reduce_dataset_minutes(directory):   
     os.makedirs(directory+"\\minutes", exist_ok=True)
     cnt = 0
+    error_cnt = 0
     for filename in os.listdir(directory):
         # only consider the dataset files
         if filename.endswith(".dat"):
             file = os.path.join(directory, filename)
             start = time.time()
-            with open(file) as f:
-                cnt += 1 # just to track progress
-                header_line = 0
-                for line in f.readlines():
-                    # Find the header_line to parse the csv 
-                    if (line[0:8] == "YYYYMMDD"):
-                        break;  # found header
-                    header_line += 1
-                df = pd.read_csv(file, sep=';\t', header=header_line, engine='python')
-                print("{} - {}s".format(cnt, time.time()-start))
-                # every second must have the same format
-                df['hhmmss'] = df['hhmmss'].astype(str).str.zfill(6)
-                df['secs'] = df['hhmmss'].astype(str).str[4:]
-                # drop unnecessary 59 seconds - just keep the first second
-                df = df[df.secs == '00']
-                newfile = directory + "\\minutes\\" + filename[:-8] + "_min.csv"
-                print(newfile)
-                # save the resulting file
-                df.to_csv(newfile, sep=';')
+            try:
+	            with open(file) as f:
+	                cnt += 1 # just to track progress
+	                header_line = 0
+	                for line in f.readlines():
+	                    # Find the header_line to parse the csv 
+	                    if (line[0:8] == "YYYYMMDD"):
+	                        break;  # found header
+	                    header_line += 1
+	                df = pd.read_csv(file, sep=';\t', header=header_line, engine='python')
+	                print("{} - {}s".format(cnt, time.time()-start))
+	                # every second must have the same format
+	                df['hhmmss'] = df['hhmmss'].astype(str).str.zfill(6)
+	                df['secs'] = df['hhmmss'].astype(str).str[4:]
+	                # drop unnecessary 59 seconds - just keep the first second
+	                df = df[df.secs == '00']
+	                newfile = directory + "\\minutes\\" + filename[:-8] + "_min.csv"
+	                print(newfile)
+	                # save the resulting file
+	                df.to_csv(newfile, sep=';')
+            except:
+	            print("!!! Unexpected Error:", sys.exc_info()[0])
+	            print("FILE: ", file)
+	            error_cnt += 1
+
+    return error_cnt
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -38,4 +47,5 @@ if __name__ == '__main__':
 
     assert os.path.isdir(args.path), "Path does not lead to a directory"
     # run the main function
-    reduce_dataset_minutes(args.path)
+    num_errors = reduce_dataset_minutes(args.path)
+    print("Finished with {} Errors!".format(num_errors))
