aff81c7c97d0d30c544ba33ac67401f7429100d9
Sebastian Widmann <widmann3@hm.edu>
2019-11-14 16:06:04 +0100

    move function to plot_utils

--- src/ecoproph.py
+++ src/ecoproph.py
@@ -8,6 +8,7 @@ import pickle
 
 import datasetutils as du
 import holidays_hm
+import plotting_utils
 
 def build_prophet_dataframe_sum(input_data, value_columns: list):
     prophet_df = pd.DataFrame()
@@ -46,31 +47,6 @@ def convert_timestamp_to_string(unixtime):
     """              
     return datetime.utcfromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S')
 
-
-def plot_forecast(df, ts_start, ts_stop, label=''):
-    """
-    @param df: forecast dataframe
-    @param ts_start: start time of the plot
-    @param ts_stop: end time of the plot
-    """   
-    # retreive the selected data
-    df['ds'] = pd.to_datetime(df['ds'])
-    df = df.set_index(['ds'])
-    df = df.loc[ts_start:ts_stop]
-    df = df.reset_index()
-
-    # plot it
-    plt.figure()
-    plt.plot(df['ds'], df['yhat'], color='blue')
-    plt.plot(df['ds'], df['yhat_lower'], color='lightblue')
-    plt.plot(df['ds'], df['yhat_upper'], color='lightblue')
-    plt.xlim((df['ds'].iloc[0], df['ds'].iloc[-1]))
-    plt.title('Forecast ' + label + ' from ' + ts_start + ' to ' + ts_stop)
-    plt.ylabel('R_BauTGb-P_SUM [kW]')
-    plt.xlabel('Date')
-    plt.xticks(rotation=25)
-    plt.fill_between(df['ds'], df['yhat_lower'], df['yhat_upper'], color='lightblue')
-    
 
 def main():
     # select and load the data
@@ -134,7 +110,7 @@ def main():
     model.plot_components(forecast)
     plt.savefig('C:\\workspace\\ecoproph\\components.png', bbox_inches='tight', dpi=500)
     
-    plot_forecast(forecast, '2019-09-01', '2019-09-30', 'R_BauTGb-P_SUM')
+    plotting_utils.plot_forecast(forecast, '2019-09-01', '2019-09-30', 'R_BauTGb-P_SUM')
     plt.savefig('C:\\workspace\\ecoproph\\my_plot.png', bbox_inches='tight', dpi=500)
 
     plt.show()
--- src/plotting_utils.py
+++ src/plotting_utils.py
@@ -20,7 +20,38 @@ def sum_a_day(df, ts_start, ts_stop):
     return df_temp
 
 
-def plot_month_forecast(df, label):
+def load_forecast_dataframe(path: str):
+    with open(path, 'rb') as fin:
+        forecast_df = pickle.load(fin)
+    return forecast_df
+
+
+def plot_forecast(df, ts_start, ts_stop, label=''):
+    """
+    @param df: forecast dataframe
+    @param ts_start: start time of the plot
+    @param ts_stop: end time of the plot
+    """   
+    # retreive the selected data
+    df['ds'] = pd.to_datetime(df['ds'])
+    df = df.set_index(['ds'])
+    df = df.loc[ts_start:ts_stop]
+    df = df.reset_index()
+
+    # plot it
+    plt.figure()
+    plt.plot(df['ds'], df['yhat'], color='blue')
+    plt.plot(df['ds'], df['yhat_lower'], color='lightblue')
+    plt.plot(df['ds'], df['yhat_upper'], color='lightblue')
+    plt.xlim((df['ds'].iloc[0], df['ds'].iloc[-1]))
+    plt.title('Forecast ' + label + ' from ' + ts_start + ' to ' + ts_stop)
+    plt.ylabel('R_BauTGb-P_SUM [kW]')
+    plt.xlabel('Date')
+    plt.xticks(rotation=25)
+    plt.fill_between(df['ds'], df['yhat_lower'], df['yhat_upper'], color='lightblue')
+
+
+def plot_month_forecast_bar(df, label):
     plt.figure()
     plt.step(df['day'], df['yhat'], color='blue')
     plt.step(df['day'], df['yhat_lower'], color='lightblue')
@@ -33,12 +64,11 @@ def plot_month_forecast(df, label):
 
 
 
+if __name__ == '__main__':
+    forecast = load_forecast_dataframe("C:\workspace\ecoprophet\forecast.pckl")
 
-with open('C:\\workspace\\ecoproph\\forecast.pckl', 'rb') as fin:
-    forecast = pickle.load(fin)
+    forecast = apply_day_column(forecast)
 
-forecast = apply_day_column(forecast)
-
-df = sum_a_day(forecast, '2019-09-01', '2019-09-30')
-plot_month_forecast(df, 'september')
-plt.show()
+    df = sum_a_day(forecast, '2019-09-01', '2019-09-30')
+    plot_month_forecast(df, 'september')
+    plt.show()
